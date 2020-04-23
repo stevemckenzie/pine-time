afb7f2e860b20a75716f5583afc976ca78a291a5
Steve McKenzie <steve@stevemckenzie.ca>
2020-04-22 21:28:17 -0700

    it begins...

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <img src="/mushroom.jpg" />
+    </div>
+  );
+}
+
+export default App;
