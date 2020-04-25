1aefee43571a0b9c4d9d3b850098944e06a834f2
Steve McKenzie <steve@stevemckenzie.ca>
2020-04-24 22:30:19 -0700

    add a starting point for an actual app layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import { Drawer, Button, Layout } from 'antd';
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+
+import Search from '../Search';
 
 import styles from './styles.module.css';
 
+const { Content, Header } = Layout;
+
 function App() {
+  const [showSearch, setShowSearch] = useState(false);
+  const onShowSearch = () => setShowSearch(true);
+  const onHideSearch = () => setShowSearch(false);
+
   return (
-    <div className={styles.app}>
-      <img src="/mushroom.jpg" />
-    </div>
+    <Layout>
+      <Header>
+        <Button onClick={onShowSearch} type="link">
+          +
+        </Button>
+      </Header>
+      <Content>content...</Content>
+      <div className={styles.app}>
+      </div>
+      <Drawer
+        onClose={onHideSearch}
+        placement="right"
+        title="Search"
+        visible={showSearch}
+      >
+        <Search />
+      </Drawer>
+    </Layout>
   );
 }
 
