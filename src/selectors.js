3c1f732a7953c1ae1bac0dea3f6ecea23e8093d1
Steve McKenzie <steve@stevemckenzie.ca>
2020-07-03 13:17:10 -0700

    add selectors for favorites and getting all stations

--- src/selectors.js
+++ src/selectors.js
@@ -1,4 +1,6 @@
 export const getStation = (id) => ({ stations: { stations = {} } }) => stations[id];
+export const getStations = ({ stations: { stations = {} } }) => stations;
 export const getStationItems = (id) => ({ stations: { items = {} } }) => items[id];
-
+export const getFavorites = ({ stations: { favorites = [] }}) => favorites;
+export const isFavorite = (id) => ({ stations: { favorites = [] }}) => favorites.includes(id);
 export const isLoading = ({ ui: { loading } }) => loading > 0;
